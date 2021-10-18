1d6c62e464f82a499f1cf54cbb608c8545a45c46
Leigh Dragoon <leighdragoon@Leigh-Dragoons-MacBook-Pro.local>
2021-10-18 14:39:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Todo-App
+
+## Synopsis
+
+Just a simple React todo-app based on MDN's [React todo list tutorial](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_todo_list_beginning).
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const DATA = [
+  {
+    id: "todo-0",
+    name: "Eat",
+    completed: true
+  },
+  {
+    id: "todo-1",
+    name: "Sleep",
+    completed: false
+  },
+  {
+    id: "todo-2",
+    name: "Repeat",
+    completed: false
+  },
+  {
+    id: "todo-3",
+    name: "Write",
+    completed: false
+  }
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
